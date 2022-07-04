92f86fd318e6878c9fb856de2015e7d0d3490ea5
Dinuka Ganganath <dinuka.ganganathr@gmail.com>
2022-07-04 21:44:27 +0530

    Admin Page Created

---
+++ Areas/Admin/Controllers/PagesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shopping_Cart.Infrastructure;
+using Shopping_Cart.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shopping_Cart.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class PagesController : Controller
+    {
+        private readonly ShoppingCartContext context;
+
+        public PagesController(ShoppingCartContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            IQueryable<Page> pages = from p in context.Pages orderby p.Sorting select p;
+            List<Page> pagesList = await pages.ToListAsync();
+            return View(pagesList);
+        }
+    }
+}
