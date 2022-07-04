1ba7a3e94fc34b3a1b9fb08af2bf116217e57efb
Dinuka Ganganath <dinuka.ganganathr@gmail.com>
2022-07-04 18:22:51 +0530

    Set up the database

---
+++ Infrastructure/ShoppingCartContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shopping_Cart.Models;
+
+namespace Shopping_Cart.Infrastructure
+{
+    public class ShoppingCartContext : DbContext
+    {
+        public ShoppingCartContext(DbContextOptions<ShoppingCartContext> options)
+            : base(options) 
+        { 
+        }
+
+        public DbSet<Page> Pages { get; set; }
+    }
+}
---
+++ Models/Page.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shopping_Cart.Models
+{
+    public class Page
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Slug { get; set; }
+        [Required]
+        public string Content { get; set; }
+        public int Sorting { get; set; }
+    }
+
+    
+}
