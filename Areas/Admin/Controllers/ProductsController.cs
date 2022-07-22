2046e856cab6c7c95204858ab0151fb553e1cbd3
Dinuka Ganganath <dinuka.ganganathr@gmail.com>
2022-07-22 20:53:13 +0530

    Product create method created

--- Areas/Admin/Controllers/ProductsController.cs
+++ Areas/Admin/Controllers/ProductsController.cs
@@ -1,7 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Shopping_Cart.Infrastructure;
+using Shopping_Cart.Models;
+using System;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,10 +15,12 @@ namespace Shopping_Cart.Areas.Admin.Controllers
     public class ProductsController : Controller
     {
         private readonly ShoppingCartContext context;
+        private readonly IWebHostEnvironment webHostEnvironment;
 
         public ProductsController(ShoppingCartContext context)
         {
             this.context = context;
+            this.webHostEnvironment = webHostEnvironment;
         }
 
         // GET: /admin/products
@@ -28,6 +34,51 @@ namespace Shopping_Cart.Areas.Admin.Controllers
         {
             ViewBag.CategoryId = new SelectList(context.Categories.OrderBy(x => x.Sorting), "Id", "Name");
             return View();
+        }
+
+        // POST: /admin/products/create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Product product)
+        {
+
+            ViewBag.CategoryId = new SelectList(context.Categories.OrderBy(x => x.Sorting), "Id", "Name");
+
+            if (ModelState.IsValid)
+            {
+                product.Slug = product.Name.ToLower().Replace(" ", "-");
+                
+                var slug = await context.Pages.FirstOrDefaultAsync(x => x.Slug == product.Slug);
+
+                if (slug != null)
+                {
+                    ModelState.AddModelError("", "The product already exists.");
+                    return View(product);
+
+                }
+
+                string imageName = "noimage.png";
+                if(product.ImageUpload != null)
+                {
+                    string uploadDir = Path.Combine(webHostEnvironment.WebRootPath, "media/products");
+                    imageName = Guid.NewGuid().ToString() + " " + product.ImageUpload.FileName;
+                    string filePath = Path.Combine(uploadDir, imageName);
+                    FileStream fs = new FileStream(filePath, FileMode.Create);
+                    await product.ImageUpload.CopyToAsync(fs);
+                    fs.Close();
+                    
+                }
+
+                product.Image = imageName;
+
+                context.Add(product);
+                await context.SaveChangesAsync();
+
+                TempData["Success"] = "The product has been added!";
+
+                return RedirectToAction("Index");
+            }
+            return View(product);
         }
     }
 }
--- Models/Product.cs
+++ Models/Product.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Shopping_Cart.Infrastructure;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -15,11 +16,14 @@ namespace Shopping_Cart.Models
         [Column(TypeName ="decimal(18, 2)")]
         public decimal Price { get; set; }
         [Display(Name = "Category")]
+        [Range(1, int.MaxValue, ErrorMessage ="You must choose a category")]
         public int CategoryId { get; set; }
+        
         public string Image { get; set; }
         [ForeignKey("CategoryId")]
         public virtual Category Category { get; set; }
         [NotMapped]
+        [FileExtension]
         public IFormFile ImageUpload { get; set; }
     }
 }
