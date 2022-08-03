730fa6ca8a39037d72cd1dddc3eae71204e4cecf
Dinuka Ganganath <dinuka.ganganathr@gmail.com>
2022-08-03 16:21:20 +0530

    Small Cart Created

---
+++ Models/SmallCartViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Shopping_Cart.Models
+{
+    public class SmallCartViewModel
+    {
+        public int NumberOfItems { get; set; }
+        public decimal TotalAmount { get; set; }
+    }
+}
