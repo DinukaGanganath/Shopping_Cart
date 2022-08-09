c023a5ada3ea492d40630e279760bbc94b832d79
Dinuka Ganganath <dinuka.ganganathr@gmail.com>
2022-08-09 14:56:42 +0530

    Role Create and Edit methods Created

---
+++ Areas/Admin/Controllers/RolesController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Shopping_Cart.Models;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+
+namespace Shopping_Cart.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class RolesController : Controller
+    {
+        private readonly RoleManager<IdentityRole> roleManager;
+        private readonly UserManager<AppUser> userManager;
+
+        public RolesController(RoleManager<IdentityRole> roleManager, UserManager<AppUser> userManager)
+        {
+            this.roleManager = roleManager;
+            this.userManager = userManager;
+        }
+
+        // GET: /admin/roles
+        public IActionResult Index() => View(roleManager.Roles);
+
+        // GET: /admin/roles/create
+        public IActionResult Create() => View();
+
+        // POST: /admin/roles/create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([MinLength(2), Required] string name)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityResult result = await roleManager.CreateAsync(new IdentityRole(name));
+                if (result.Succeeded)
+                {
+                    TempData["Success"] = "The role has been created";
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    foreach (IdentityError error in result.Errors) ModelState.AddModelError("", error.Description);
+                }
+                    
+            }
+
+            ModelState.AddModelError("", "Minimum length is 2");
+            return View();
+        }
+
+        // POST: /admin/roles/edit
+        public async Task<IActionResult> Edit(string id)
+        {
+            IdentityRole role = await roleManager.FindByIdAsync(id);
+
+            List<AppUser> members = new List<AppUser>();
+            List<AppUser> nonMembers = new List<AppUser>();
+
+            foreach (AppUser user in userManager.Users)
+            {
+                var list = await userManager.IsInRoleAsync(user, role.Name) ? members : nonMembers;
+                list.Add(user);
+            }
+            return View(new RoleEdit
+            {
+                Role = role,
+                Members = members,
+                NonMembers = nonMembers
+            });
+        }
+    }
+}
---
+++ Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Shopping_Cart.Models;
+
+namespace Shopping_Cart.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+        private readonly UserManager<AppUser> userManager;
+
+        public UserController(UserManager<AppUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+        public IActionResult Index()
+        {
+            return View(userManager.Users);
+        }
+    }
+}
